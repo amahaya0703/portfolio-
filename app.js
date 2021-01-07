e6f704c8342c1d2a0a2e3d09e600f1de5c33ef63
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2021-01-07 12:07:31 +0900

    修正part2

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ const parent = document.querySelector(".parent");
 hamberger.addEventListener("click", () => {
   parent.classList.toggle("action");
 });
+
+// // const show = document.getElementById("show");
+
+// // const nav = document.querySelector(".nav_link");
+
+// // show.addEventListener("click", () => {
+// //   nav.classList.toggle("action");
+// // });
+
+// const nav = document.querySelectorAll(".nav_link");
+
+// function LinkAction() {
+//   const show = document.getElementById("show");
+//   show.classList.toggle("active");
+// }
+
+// nav.forEach((n) => n.addEventListener("click", LinkAction));
